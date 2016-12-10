461ea641872d83600617114317d87303fa1fa2d8
Wouter de Bres <wouter@breadandpepper.com>
2016-12-10 13:37:37 +0100

    Working on cover, adding chapters

--- static/scripts/src/main.js
+++ static/scripts/src/main.js
@@ -1,9 +1,18 @@
-// To show the grid, press Enter on keyboard
-$(document).keypress(function(e) {
-  if(e.which == 13) {
-    $('#grid').toggleClass('show').css('height', $(document).height());
-  }
-});
+// To make arrow keys go to next and prev chapters
+    $(document).ready(function() {
+            document.onkeydown = function() 
+                {
+                    var j = event.keyIdentifier
+                    if (j == "Right")
+                        window.location = nextUrl
+                    else if (j == "Left")
+                        window.location = prevUrl            
+                        }
+                   });
 
-$(document).ready(function(){
-});
+      $(document).ready(function() {
+                    var nextPage = $("#next-page")
+                    var prevPage = $("#prev-page")
+                    nextUrl = nextPage.attr("href")
+                    prevUrl = prevPage.attr("href")
+                });
